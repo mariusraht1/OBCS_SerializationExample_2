99f56faf32880d54ce9490f53da0c3801c373023
mariusraht2 <mariusraht+2@gmail.com>
2020-01-25 14:23:46 +0100

    Implemented

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PurchaseOrderLib;
+using System;
 
 namespace ConsoleApp
 {
@@ -6,7 +7,26 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Purchaser purchaser = new Purchaser("Schuler", "Peter M.", "Universitätsstr. 9", "Essen", 45141);
+
+            //Item[] items = new Item[2];
+            //Item item1 = new Item("Braun WK210 Sahara", "Wasserkocher - kabellos", 30.00m, 5);
+            //Item item2 = new Item("Philips HP 4841 Compact", "Haartrockner - kabelgebunden", 12.50m, 2);
+
+            //items[0] = item1;
+            //items[1] = item2;
+
+            //PurchaseOrder purchaseOrder = new PurchaseOrder(purchaser, "2006-02-09", items, 6.95m);
+
+            Serializer serializer = new Serializer();
+
+            PurchaseOrder purchaseOrder = serializer.ImportFromXml("output.xml");
+            serializer.ExportToXml("output.xml", purchaseOrder);
+
+            Console.WriteLine(purchaseOrder.ToString());
+
+            Console.WriteLine("Done.");
+            Console.ReadLine();
         }
     }
 }
---
+++ PurchaseOrder/PurchaseOrder.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace PurchaseOrderLib
+{
+    public class PurchaseOrder
+    {
+        public Purchaser Purchaser { get; set; }
+        public string OrderDate { get; set; }
+        [XmlArray("Items")]
+        [XmlArrayItem(ElementName = "OrderedItem", Type = typeof(Item))]
+        public Item[] OrderedItems { get; set; }
+        public decimal ShipCost { get; set; }
+
+        private decimal subTotal;
+        public decimal SubTotal
+        {
+            get
+            {
+                subTotal = 0;
+
+                for (int i = 0; i < OrderedItems.Length; i++)
+                {
+                    subTotal += OrderedItems[i].LineTotal;
+                }
+
+                return subTotal;
+            }
+            set
+            {
+                subTotal = value;
+            }
+        }
+
+        private decimal totalCost;
+        public decimal TotalCost
+        {
+            get
+            {
+                totalCost = SubTotal + ShipCost;
+                return totalCost;
+            }
+            set
+            {
+                totalCost = value;
+            }
+        }
+
+        public PurchaseOrder() { }
+
+        public PurchaseOrder(Purchaser purchaser, string orderDate, Item[] orderedItems, decimal shipCost)
+        {
+            Purchaser = purchaser;
+            OrderDate = orderDate;
+            OrderedItems = orderedItems;
+            ShipCost = shipCost;
+        }
+
+        public override string ToString()
+        {
+            return $"Sub Total: {SubTotal}\nShip Cost: {ShipCost}\nTotal Cost: {TotalCost}";
+        }
+    }
+
+    [XmlRoot("ShipTo")]
+    public class Purchaser
+    {
+        [XmlAttribute]
+        public string Surname { get; set; }
+        [XmlAttribute]
+        public string Forename { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public int Zip { get; set; }
+
+        public Purchaser() { }
+
+        public Purchaser(string surname, string forename, string street, string city, int zip)
+        {
+            Surname = surname;
+            Forename = forename;
+            Street = street;
+            City = city;
+            Zip = zip;
+        }
+    }
+
+    public class Item
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+
+        private decimal lineTotal;
+        public decimal LineTotal
+        {
+            get
+            {
+                lineTotal = UnitPrice * Quantity;
+                return lineTotal;
+            }
+            set
+            {
+                lineTotal = value;
+            }
+        }
+
+        public Item() { }
+
+        public Item(string name, string description, decimal unitPrice, int quantity)
+        {
+            Name = name;
+            Description = description;
+            UnitPrice = unitPrice;
+            Quantity = quantity;
+        }
+    }
+
+    public class Serializer
+    {
+        public void ExportToXml(string fileName, PurchaseOrder purchaseOrder)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(PurchaseOrder));
+
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                serializer.Serialize(writer, purchaseOrder);
+            }
+        }
+
+        public PurchaseOrder ImportFromXml(string fileName)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(PurchaseOrder));
+
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                return (PurchaseOrder)serializer.Deserialize(reader);
+            }
+        }
+    }
+}
